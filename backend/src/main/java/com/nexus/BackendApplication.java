fc533c8936f386322985f9283cb77b746a96512f
L. Torres <ltorres@nidix.mx>
2026-09-18 17:57:26 -0600

    Comentario de prueba

--- backend/src/main/java/com/nexus/BackendApplication.java
+++ backend/src/main/java/com/nexus/BackendApplication.java
@@ -3,6 +3,7 @@ package com.nexus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Esto es una prueba de commit.
 @SpringBootApplication
 public class BackendApplication {
 
